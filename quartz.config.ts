86e7bc00ce62e9afb9011f0c98eb923d7d3aabcf
Sergio <sergiogz@protonmail.com>
2025-03-08 19:03:09 +0000

    quartz troubleshooting

--- quartz.config.ts
+++ quartz.config.ts
@@ -4,7 +4,7 @@ import * as Plugin from "./quartz/plugins"
 /**
  * Quartz 4 Configuration
  *
- * See https://quartz.jzhao.xyz/configuration for more information.
+ * 
  */
 const config: QuartzConfig = {
   configuration: {
